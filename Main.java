7aeeb37029afeba35de4c85ce9472c5c72eafe60
hm-imu-sc <hmimusc850@gmail.com>
2022-01-06 09:23:50 +0600

    created SharedMemory class and added right():void method

---
+++ Main.java
@@ -0,0 +1,7 @@
+
+class Main {
+    public static void main(String[] args) {
+        SharedMemory x = new SharedMemory();
+        x.write("address", "value");
+    }
+}
---
+++ SharedMemory.java
@@ -0,0 +1,16 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SharedMemory {
+
+    private Map<Object, Object> memory;
+    
+    SharedMemory () {
+        this.memory = new HashMap<>();
+    }
+
+    public void write(Object address, Object value) {
+        this.memory.put(address, value);
+    }
+}
